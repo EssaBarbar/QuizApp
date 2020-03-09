2851b782453f27b90b247ce7720c3bccfa1e6356
Essa <essa992@gmail.com>
2020-03-09 11:33:09 +0100

    changed to html

--- handler.js
+++ handler.js
@@ -12,50 +12,50 @@ function makeRequest(url, method, data, callback) {
 }
 // document.getElementById("submitBtn").addEventListener("click", postScore()); för eve
 
-function postScore(){
-      
-        var username = document.getElementById("userName").value
-        //var lName = document.getElementById("lName").value <--- score
-       
-    
-        let data = new FormData()
-        data.append("Name", username)
-        data.append("Score", score)
-        data.append("endpoint", "postScore")
-        
-                
-        makeRequest('./databasereciever.php', 'POST', data, (result) => {
-        })        
-    
+function postScore() {
+
+    var username = document.getElementById("userName").value
+    //var lName = document.getElementById("lName").value <--- score
+
+
+    let data = new FormData()
+    data.append("Name", username)
+    data.append("Score", score)
+    data.append("endpoint", "postScore")
+
+
+    makeRequest('./databasereciever.php', 'POST', data, (result) => {
+    })
+
 }
 
 function getHighScores() {
-    
+
     makeRequest('./databasereciever.php?endpoint=getScore', 'GET', null, (result) => {
         console.log(result)
-        if (result.status == 404){
+        if (result.status == 404) {
         } else {
-            renderHighScores(result);     
+            renderHighScores(result);
         }
     })
 }
 
 
-function renderHighScores(result){
+function renderHighScores(result) {
 
-    
-        const showScores = document.getElementById("scoreList")
-        
 
-        for (let i = 0; i < result.length; i++) {
-            const selectedScore = result[i]
-            
-            const scoreListItem = document.createElement("li")
-            scoreListItem.classList = "scoreListItem"
-            scoreListItem.innerHTML = selectedScore.name + " " + selectedScore.score
-            showScores.append(scoreListItem)            
+    const showScores = document.getElementById("scoreList")
+
+
+    for (let i = 0; i < result.length; i++) {
+        const selectedScore = result[i]
+
+        const scoreListItem = document.createElement("li")
+        scoreListItem.classList = "scoreListItem"
+        scoreListItem.innerHTML = selectedScore.name + " " + selectedScore.score
+        showScores.append(scoreListItem)
     }
-    
+
 }
 
 getHighScores();
